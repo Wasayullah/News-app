f61976e00e62228f826843e4edfaf8ce38a595b0
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-07-26 19:34:31 +0000

    Refactor news fetching logic and enhance HTML structure for better layout and responsiveness

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-let search = document.getElementById("search").value
+let search = document.getElementById("search")
 let getNews = document.getElementById("main")
 let findNews = () => {
-    fetch(`https://newsapi.org/v2/everything?q=${search}&language=en&sortBy=publishedAt&pageSize=20&apiKey=8623961ef42042b3a6773c81a381262d`)
+    getNews.innerHTML = ''
+    fetch(`https://newsapi.org/v2/everything?q=${search.value}&language=en&sortBy=publishedAt&pageSize=20&apiKey=8623961ef42042b3a6773c81a381262d`)
         .then((data) => data.json())
         .then((data) => {
             data.articles.map((value) => {
+            
             getNews.innerHTML += `
             <div class="card" style="width: 18rem;">
                 <img src="${value.urlToImage}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">${value.title}</h5>
+                    <p class="card-text">${value.description}</p>
+                    <a href="${value.url}" class="btn btn-primary" target="_blank">Read more</a>
                 </div>
             </div>
         `
             })
         })
+
 }
